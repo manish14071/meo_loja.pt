d8d55c582e770583837a18588c66be15337da316
manish -- <manish--@manishs-MacBook-Pro-2.local>
2025-02-10 00:12:45 +0000

    bdg

--- Server/middleware/securityMiddleware.js
+++ Server/middleware/securityMiddleware.js
@@ -16,7 +16,7 @@ export const xssProtection = xss();
 
 // CORS options
 export const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3267'],
+  origin: ['http://localhost:5173', 'http://localhost:3267',"https://meo-loja-pt.onrender.com/"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
